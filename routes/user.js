df5a03c3ad3d4882ced77bef99aa571340c5ef24
Mounir Ben Kasmiya <mounir.benkasmiya@gmail.com>
2021-03-06 11:21:23 +0100

    add function jwtverify

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authVerification = require("../config/authentification");
 const userController = require("../controllers/userController");
 
-router.get("/users", userController.getUsers);
+router.get("/users", authVerification, userController.getUsers);
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 
